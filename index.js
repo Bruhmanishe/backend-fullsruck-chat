4be14286990ae501c43da197d3d3ac52690a8ea8
Bruhmanishe <158217866+Bruhmanishe@users.noreply.github.com>
2025-06-08 03:16:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: 
-      "https://fullstuck-chat-render-client.onrender.com/",
+      "https://fullstuck-chat-render-client.onrender.com",
     credentials: true,
   })
 );
